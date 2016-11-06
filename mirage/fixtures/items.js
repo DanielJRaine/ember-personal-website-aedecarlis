196d44e41ac41e3110cd0d18c6049dd728d5061c
danieljraine <ley.danielj@gmail.com>
2016-11-06 15:56:47 -0500

    make routes with links from top to bottom layer

--- mirage/fixtures/items.js
+++ mirage/fixtures/items.js
@@ -1,7 +1,18 @@
 export default [
-  { name: "portrait-1" },
-  { data: "image-data-goes-here" },
-  { artform: "visual" },
-  { portfolio: "fine-arts" },
-  { section: "portraits" },
+  {
+    id: 1,
+    name: "portrait-1",
+    data: "image-data-goes-here",
+    artform: "visual",
+    portfolio: "fine-arts",
+    section: "portraits",
+ },
+ {
+   id: 2,
+   name: "rainemaker",
+   data: "makin' it rain",
+   artform: "visual",
+   portfolio: "graphic-design",
+   section: "logos"
+ },
 ];
--- mirage/fixtures/sections.js
+++ mirage/fixtures/sections.js
@@ -1,5 +1,12 @@
 export default [
-  { name: 'portraits' },
-  { artform: 'visual' },
-  { items: ['portrait-1']  },
+  {
+    name: 'portraits',
+    artform: 'visual',
+    items: ['portrait-1'],
+  },
+  {
+    name: 'logos',
+    artform: 'visual',
+    items: ['rainemaker, freshwagon'],
+  },
 ];
