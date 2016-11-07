cad21a7dbb6e827a9470215fa99bbc35e12a377a
danieljraine <ley.danielj@gmail.com>
2016-11-06 21:03:52 -0500

    Figure out data-down with models from routes

--- app/visual/route.js
+++ app/visual/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 // Visual route
 export default Ember.Route.extend({
   model() {
-    this.get('store').findAll('portfolio');
+    return this.get('store').findAll('portfolio');
   }
 });
