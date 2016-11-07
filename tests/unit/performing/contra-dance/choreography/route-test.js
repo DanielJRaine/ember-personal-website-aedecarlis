a24cf12e8127ae644b890cca294c59d378ffabcb
danieljraine <ley.danielj@gmail.com>
2016-11-06 19:31:32 -0500

    Add all of the routes

---
+++ tests/unit/performing/contra-dance/choreography/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:performing/contra-dance/choreography', 'Unit | Route | performing/contra dance/choreography', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
