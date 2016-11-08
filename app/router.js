5351f8776d510e358076f707ca618ff823d36de1
danieljraine <ley.danielj@gmail.com>
2016-11-08 10:59:42 -0500

    delete everything

--- app/router.js
+++ app/router.js
@@ -10,28 +10,6 @@ Router.map(function () {
   this.route('sign-in');
   this.route('change-password');
   this.route('users');
-  this.route('visual', function() {
-    this.route('graphic-design', function() {
-      this.route('logos');
-    });
-    this.route('art-tutor');
-    this.route('art-model');
-    this.route('wedding-and-event-drawing');
-    this.route('fine-arts', function() {
-      this.route('portraits');
-      this.route('musician-portraits');
-      this.route('other');
-    });
-  });
-  this.route('bio');
-  this.route('contact');
-  this.route('performing', function() {
-    this.route('contra-dance', function() {
-      this.route('choreography');
-      this.route('caller');
-    });
-    this.route('jazz');
-  });
 });
 
 export default Router;
--- app/visual/fine-arts/route.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-// fine-arts portfolio
-export default Ember.Route.extend({
-  model() {
-    return this.get('store').findAll('section');
-  }
-});
