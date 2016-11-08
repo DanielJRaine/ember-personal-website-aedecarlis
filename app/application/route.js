70971bbffa96b32cc1a711f60f998dc68b80fa13
danieljraine <ley.danielj@gmail.com>
2016-11-08 11:44:42 -0500

    find routes from attributes on item in db

--- app/application/route.js
+++ app/application/route.js
@@ -3,11 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
   flashMessages: Ember.inject.service(),
-
+  model () {
+    return this.get('store').findAll('item');
+  },
   actions: {
-    // loadLinks () {
-    //   this.get('')
-    // },
     signOut () {
       this.get('auth').signOut()
       .then(() => this.transitionTo('sign-in'))
