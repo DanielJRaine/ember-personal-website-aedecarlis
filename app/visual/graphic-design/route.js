32f9ecf7ef6d888071603ad53fd2cc2f4f21ebbc
danieljraine <ley.danielj@gmail.com>
2016-11-08 10:59:42 -0500

    delete everything

--- app/visual/graphic-design/route.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model() {
-    // find a way to extract the portfolio out of item
-    return this.get('store').findAll('section');
-  }
-});
