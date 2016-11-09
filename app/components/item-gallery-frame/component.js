1953e3cb9871980ce28daea357cc16a01c650d2c
danieljraine <ley.danielj@gmail.com>
2016-11-09 13:42:56 -0500

    nest components. more actions down, data up

--- app/components/item-gallery-frame/component.js
+++ app/components/item-gallery-frame/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['col-lg-4'],
+  classNames: ['col-lg-6'],
   actions: {
     delete() {
       this.sendAction('delete', this.get('item'));
