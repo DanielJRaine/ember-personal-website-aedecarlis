3808c7e83ebf7943663787ada3213c36191d5d3d
danieljraine <ley.danielj@gmail.com>
2016-11-09 18:27:52 -0500

    hardcode links

--- app/components/my-application/component.js
+++ app/components/my-application/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   auth: Ember.inject.service(),
-
+  navItems: Ember.computed.uniqBy('items', 'artform'),
   user: Ember.computed.alias('auth.credentials.email'),
   isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
 
