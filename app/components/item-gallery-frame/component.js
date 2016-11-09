e0957cb48ae752273718eb3b5a3a8fdfe1873030
danieljraine <ley.danielj@gmail.com>
2016-11-09 13:54:44 -0500

    Make responsive

--- app/components/item-gallery-frame/component.js
+++ app/components/item-gallery-frame/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['col-lg-6'],
+  classNames: ['col-lg-6', 'col-md-6', 'col-sm-12', 'col-xs-12'],
   actions: {
     delete() {
       this.sendAction('delete', this.get('item'));
