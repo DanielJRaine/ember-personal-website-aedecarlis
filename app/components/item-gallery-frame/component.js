9a492ba2f4f277779d89abd79384195ce911d9fd
danieljraine <ley.danielj@gmail.com>
2016-11-09 13:16:06 -0500

    place items in rows

--- app/components/item-gallery-frame/component.js
+++ app/components/item-gallery-frame/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-
+  classNames: ['col-lg-4'],
   actions: {
     delete() {
       this.sendAction('delete', this.get('item'));
